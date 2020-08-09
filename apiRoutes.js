30c1b0ec53b6b35c24b64927e4393ca00292681f
Tamar <mischegoss@gmail.com>
2020-08-09 13:11:09 -0400

    Add store to routes

--- apiRoutes.js
+++ apiRoutes.js
@@ -3,7 +3,14 @@ var store = require("../db/store");
 
 // GET "/api/notes" responds with all notes from the database
 router.get("/notes", function(req, res) {
-  // We will get this stuff from the store
+
+  store
+    .getNotes()
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json(err));
+  
 });
 
 module.exports = router;
+
+
